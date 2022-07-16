c49668181a82c7a9f3712e7afa3dbb959962170e
currentScript <jannisull@gmail.com>
2022-07-16 18:11:54 +0200

    rounded taglist corners

--- .config/awesome/themes/spaceman/theme.lua
+++ .config/awesome/themes/spaceman/theme.lua
@@ -1,14 +1,19 @@
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
+local gears_shape = require("gears.shape")
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 
+local taglistShape = function(cr, width, height)
+    gears_shape.rounded_rect(cr, width, height, 4)
+end
+
 local theme = {}
 
 local bgColor 		= "#191919"	-- dark gray
-local accent 	    = "#e95678"
+local accent 	    = "#e95678" -- red
 
 theme.font		    = "mononoki"
 
@@ -29,6 +34,8 @@ theme.border_width	= dpi(2)
 theme.border_normal	= "#000000"
 theme.border_focus	= accent
 theme.border_marked	= accent
+
+theme.taglist_shape = taglistShape 
 
 theme.wallpaper		= "/usr/share/pixmaps/spaceman.jpeg"
 theme.icon_theme	= nil
