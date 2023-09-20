2348a6e3576b5a2ea270ba848694aaed6116aac5
currentScript <jannisull@gmail.com>
2023-09-20 08:36:29 +0200

    changes to statusline

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,14 +1,22 @@
 local lualineTheme = require('lualine.themes.base16')
-lualineTheme.normal.a.bg = "#7cd4f0" --blue
-lualineTheme.insert.a.bg = "#85f07c" --green
-lualineTheme.visual.a.bg = "#f0bb7c" --orange/yellow
-lualineTheme.replace.a.bg = "#f07d7c" --red
+lualineTheme.normal.a.bg = "#7cd4f0"   --blue
+lualineTheme.insert.a.bg = "#85f07c"   --green
+lualineTheme.visual.a.bg = "#f0bb7c"   --orange/yellow
+lualineTheme.replace.a.bg = "#f07d7c"  --red
 lualineTheme.inactive.a.bg = "#3f475a" --gray
 
 require("lualine").setup {
     options = {
         theme = lualineTheme
-    }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
 
 vim.o.cursorline = true
